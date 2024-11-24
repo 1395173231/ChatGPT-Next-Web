c056356497a3ddd772bbbb5d2f26df8a0b8018f8
AhhhLiu <s1395173231@gmail.com>
2024-11-24 21:54:39 +0800

    将serviceWorker转换为es5提示兼容性

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,62 +1,82 @@
-const CHATGPT_NEXT_WEB_CACHE = "chatgpt-next-web-cache";
-const CHATGPT_NEXT_WEB_FILE_CACHE = "chatgpt-next-web-file";
-let a="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";let nanoid=(e=21)=>{let t="",r=crypto.getRandomValues(new Uint8Array(e));for(let n=0;n<e;n++)t+=a[63&r[n]];return t};
+var CHATGPT_NEXT_WEB_CACHE = "chatgpt-next-web-cache";
+var CHATGPT_NEXT_WEB_FILE_CACHE = "chatgpt-next-web-file";
+var a = "useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";
+var nanoid = function (e) {
+  if (e === void 0) e = 21;
+  var t = "";
+  var r = crypto.getRandomValues(new Uint8Array(e));
+  for (var n = 0; n < e; n++) {
+    t += a[63 & r[n]];
+  }
+  return t;
+};
 
 self.addEventListener("activate", function (event) {
   console.log("ServiceWorker activated.");
 });
 
 self.addEventListener("install", function (event) {
-  self.skipWaiting();  // enable new version
+  self.skipWaiting(); // enable new version
   event.waitUntil(
     caches.open(CHATGPT_NEXT_WEB_CACHE).then(function (cache) {
       return cache.addAll([]);
-    }),
+    })
   );
 });
 
 function jsonify(data) {
-  return new Response(JSON.stringify(data), { headers: { 'content-type': 'application/json' } })
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" }
+  });
 }
 
-async function upload(request, url) {
-  const formData = await request.formData()
-  const file = formData.getAll('file')[0]
-  let ext = file.name.split('.').pop()
-  if (ext === 'blob') {
-    ext = file.type.split('/').pop()
-  }
-  const fileUrl = `${url.origin}/api/cache/${nanoid()}.${ext}`
-  // console.debug('file', file, fileUrl, request)
-  const cache = await caches.open(CHATGPT_NEXT_WEB_FILE_CACHE)
-  await cache.put(new Request(fileUrl), new Response(file, {
-    headers: {
-      'content-type': file.type,
-      'content-length': file.size,
-      'cache-control': 'no-cache', // file already store in disk
-      'server': 'ServiceWorker',
+function upload(request, url) {
+  return request.formData().then(function (formData) {
+    var file = formData.getAll("file")[0];
+    var ext = file.name.split(".").pop();
+    if (ext === "blob") {
+      ext = file.type.split("/").pop();
     }
-  }))
-  return jsonify({ code: 0, data: fileUrl })
+    var fileUrl = url.origin + "/api/cache/" + nanoid() + "." + ext;
+    return caches.open(CHATGPT_NEXT_WEB_FILE_CACHE).then(function (cache) {
+      return cache
+        .put(
+          new Request(fileUrl),
+          new Response(file, {
+            headers: {
+              "content-type": file.type,
+              "content-length": file.size,
+              "cache-control": "no-cache", // file already stored on disk
+              server: "ServiceWorker"
+            }
+          })
+        )
+        .then(function () {
+          return jsonify({ code: 0, data: fileUrl });
+        });
+    });
+  });
 }
 
-async function remove(request, url) {
-  const cache = await caches.open(CHATGPT_NEXT_WEB_FILE_CACHE)
-  const res = await cache.delete(request.url)
-  return jsonify({ code: 0 })
+function remove(request, url) {
+  return caches.open(CHATGPT_NEXT_WEB_FILE_CACHE).then(function (cache) {
+    return cache.delete(request.url).then(function (res) {
+      return jsonify({ code: 0 });
+    });
+  });
 }
 
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
+self.addEventListener("fetch", function (e) {
+  var url = new URL(e.request.url);
   if (/^\/api\/cache/.test(url.pathname)) {
-    if ('GET' == e.request.method) {
-      e.respondWith(caches.match(e.request))
+    if ("GET" === e.request.method) {
+      e.respondWith(caches.match(e.request));
     }
-    if ('POST' == e.request.method) {
-      e.respondWith(upload(e.request, url))
+    if ("POST" === e.request.method) {
+      e.respondWith(upload(e.request, url));
     }
-    if ('DELETE' == e.request.method) {
-      e.respondWith(remove(e.request, url))
+    if ("DELETE" === e.request.method) {
+      e.respondWith(remove(e.request, url));
     }
   }
 });
